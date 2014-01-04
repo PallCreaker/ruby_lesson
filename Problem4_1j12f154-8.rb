688903dcd222f9cc5d0790b158546cfbe24f8c37
iMac for local <pallcreaker@gmail.com>
2014-01-04 12:44:37 +0900

    colored

--- Problem4_1j12f154-8.rb
+++ Problem4_1j12f154-8.rb
@@ -1,12 +1,13 @@
 # coding: utf-8
 require 'timeout'
+require 'colored'
 
-def random_words_hit_game()
-    p "この問題の説明・・・・・・"
+def random_words_hit_game( time = 20 )
+    puts "\n英単語を当てるゲームです。\n\n英単語の語の順序がバラバラになっているので、それを正しく順番を直して入力してください。\n\nなお、以下の問題は#{time}秒以内に答えてください。間違えるとヒントも少しあります。\n\nそれでは10秒後にスタートします。"
+    sleep 10
     filename = ARGV[0]                 # コマンドの第1引数を filname に代入
     file = open(filename)              # filname にある名前のファイルを開く
     i = 1
-    time = 2
     miss_count = 0
     qestion_count = 10
     while fileText = file.gets()           # file から1行読み込み
@@ -14,9 +15,9 @@ def random_words_hit_game()
         hint_count = 1
         problem = fileText.split(",")
         qestion_words = problem[0].split(//).shuffle
-        p "第#{i}問。以下の語を並べ替えて、意味のある英単語にしなさい"
+        puts " ### 第#{i}問 ###".yellow
         qestion_words.each do |word|
-            print " #{word} "
+            print " #{word} ".green
         end
         puts "\n"
         i += 1
@@ -27,31 +28,31 @@ def random_words_hit_game()
                     if a_words.chomp == problem[0]
                         puts message = "good!"
                     elsif hint_count > 2
-                        p "もうヒントはありません"
+                        puts "もうヒントはありません"
                     else
-                        p "はずれです。ヒントを見ますか?見る方は'yes'を見ない方は'no'を入力してください。"
+                        puts "はずれです。\nヒントを見ますか?見る方は'yes'を見ない方は'no'を入力してください。"
                         hint_if = STDIN.gets()
                         if hint_if.chomp == "yes"
-                            p "これで最後のヒントです。" if hint_count == 2
-                            p "ヒントは #{problem[hint_count].chomp}"
+                            puts "これで最後のヒントです。" if hint_count == 2
+                            puts "ヒントは #{problem[hint_count].chomp}"
                             hint_count += 1
                         elsif hint_if.chomp == "no"
                             next
                         else
-                            p "yes か no でお願いします。"
+                            puts "yes か no でお願いします。"
                         end
                     end
                 end
             }
         rescue Timeout::Error
-            puts "Timeout!"
+            puts "Timeout!".red
             miss_count += 1
         end
     end
     ans_rate = ((qestion_count - miss_count) * 100 ) / qestion_count
-    p "終了！"
-    p "あなたの正解率は？#{ans_rate} %"
+    puts "終了！"
+    puts "あなたの正解率は？\n #{ans_rate} %".red.bold
     file.close                        # ファイルを閉じる
 end
 
-random_words_hit_game()
+random_words_hit_game(1)
